494974e7eb479de834179d33efb6f6c7914af6c5
Eghosa <161533129+eghosa40@users.noreply.github.com>
2026-08-18 10:12:53 +0100

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            int missingElement = target - nums[i];
+            if(map.containsKey(missingElement) && map.get(missingElement) != i){
+                return new int[]{i, map.get(missingElement)};
+            }
+        }
+        return new int[]{0,0};
+    }
+}
