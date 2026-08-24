642b52f66921a32e2a072a13ad3a16d7e9c6a490
Eghosa <161533129+eghosa40@users.noreply.github.com>
2026-08-24 17:50:01 +0100

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(']', '[');
+        map.put('}', '{');
+        map.put(')', '(');
+
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    if(map.get(c) == stack.peek()){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
